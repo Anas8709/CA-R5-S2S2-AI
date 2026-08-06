f56b10e0984e8425711033e5b20e6b2f23715856
Anas <anasmalek11123@gmail.com>
2026-08-06 22:03:01 +0300

    Session 3

---
+++ src/python/session 3/code/info_user.py
@@ -0,0 +1,12 @@
+def user_info(name , age):
+    ''' 
+    breif description of your function
+    
+    Args:
+        param_name(str): this is string input for name of user
+        param_age(int): this is int input for age of user
+        
+    return:
+        return_type: print the description for user info
+    '''
+    print(f"my name is {name} , my age is {age}")
